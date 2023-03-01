67caf51f07c0bd14fe35cac24bc8589e79ed17da
beta <santibeta82@gmail.com>
2023-02-28 23:50:10 -0500

    now mobile menu closes when cliking on a link

--- js/headerMenu.js
+++ js/headerMenu.js
@@ -1,6 +1,7 @@
 const openBtn = document.querySelector(".header__menuBtn");
 const menuContainer = document.querySelector(".header__nav__list__container");
 const menu = document.querySelector(".header__nav__list");
+const menuLink = document.querySelectorAll(".header__nav__link");
 
 const closeBtn = document.querySelector(".header__closeBtn");
 
@@ -19,5 +20,20 @@ closeBtn.addEventListener("click", () => {
     menuContainer.classList.remove("header__nav__list__container--active");
     menuContainer.classList.remove("fade-out");
   }, 280);
+});
+
+menuLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.add("slide-out");
+    setTimeout(() => {
+      menuContainer.classList.add("fade-out");
+    }, 200);
+
+    setTimeout(() => {
+      menu.classList.remove("slide-out");
+      menuContainer.classList.remove("header__nav__list__container--active");
+      menuContainer.classList.remove("fade-out");
+    }, 280);
+  });
 });
 
